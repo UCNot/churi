a8d978ccca61d8a0594213e1f20814ec41af5098
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-02-17 12:29:18 +0700

    Drop debug output

--- src/compiler/deserialization/ucd-lib.ts
+++ src/compiler/deserialization/ucd-lib.ts
@@ -148,8 +148,6 @@ export class UcdLib<TSchemae extends UcdLib.Schemae = UcdLib.Schemae> {
   async #toDeserializers(): Promise<UcdLib.Exports<TSchemae>> {
     const code = new UccCode().write(this.#toFactoryCode()).toString();
 
-    console.debug(code);
-
     // eslint-disable-next-line @typescript-eslint/no-implied-eval
     const factory = Function(code) as () => Promise<UcdLib.Exports<TSchemae>>;
 
