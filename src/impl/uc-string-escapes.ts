66d0a51df1b5fc6de3b4959d0d9c26b64b478b38
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-04-08 10:20:06 +0700

    Remove unused code

--- src/impl/uc-string-escapes.ts
+++ src/impl/uc-string-escapes.ts
@@ -1,18 +1,8 @@
 import { ASCIICharSet } from './ascii-char-set.js';
 
-export function escapeUcValue(encoded: string): string {
-  const escaped = escapeUcSpecials(encoded);
-
-  return UC_ESCAPED.prefixes(escaped) ? `'${escaped}` : escaped;
-}
-
 export const UC_ESCAPED = /*#__PURE__*/ new ASCIICharSet("!$'-0123456789");
 
 export const UC_KEY_ESCAPED = /*#__PURE__*/ new ASCIICharSet("!$'");
-
-export function escapeUcKey(encoded: string): string {
-  return prefixUcKey(escapeUcSpecials(encoded));
-}
 
 export function prefixUcKey(escaped: string): string {
   return UC_KEY_ESCAPED.prefixes(escaped) ? `$${escaped}` : escaped;
