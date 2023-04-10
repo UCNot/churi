37dbd1aad0fbe3b18f77025132859194ff139550
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-04-10 18:39:57 +0700

    Rename file

--- src/compiler/deserialization/mod.ts
+++ src/compiler/deserialization/mod.ts
@@ -1,7 +1,6 @@
 export * from './list.ucrx-template.js';
 export * from './map.ucrx-entry.js';
 export * from './map.ucrx-template.js';
-export * from './ucd-enable-primitives.js';
 export * from './ucd-entity-feature.js';
 export * from './ucd-feature.js';
 export * from './ucd-function.js';
@@ -9,3 +8,4 @@ export * from './ucd-lib.js';
 export * from './ucd-support-basic.js';
 export * from './ucd-support-defaults.js';
 export * from './ucd-support-non-finite.js';
+export * from './ucd-support-primitives.js';
--- src/compiler/deserialization/ucd-support-basic.ts
+++ src/compiler/deserialization/ucd-support-basic.ts
@@ -1,7 +1,7 @@
 import { ListUcrxTemplate } from './list.ucrx-template.js';
 import { MapUcrxTemplate } from './map.ucrx-template.js';
-import { ucdSupportPrimitives } from './ucd-enable-primitives.js';
 import { UcdSetup } from './ucd-feature.js';
+import { ucdSupportPrimitives } from './ucd-support-primitives.js';
 import { UnknownUcrxTemplate } from './unknown.ucrx-template.js';
 
 export function ucdSupportBasic(setup: UcdSetup): void {
