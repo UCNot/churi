ac40d2bcc21e2026eb46878ec0c2f29eff5cd808
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2023-07-13 13:15:17 +0700

    Bundle schema processors by bundle references (#58)

--- src/schema/uc-bundle.spec.ts
+++ src/schema/uc-bundle.spec.ts
@@ -1,24 +1,15 @@
 import { describe, expect, it } from '@jest/globals';
-import { createUcSerializer } from 'churi';
-import { ucString } from './string/uc-string.js';
 import { createUcBundle } from './uc-bundle.js';
-import { createUcDeserializer } from './uc-deserializer.js';
 
 describe('UcBundle', () => {
-  it('returns the bundle itself', () => {
+  it('returns bundle representation', () => {
     expect(
       createUcBundle({
         dist: './my-bundle.js',
-        bundle() {
-          return {
-            readValue: createUcDeserializer(ucString()),
-            writeValue: createUcSerializer(ucString()),
-          };
-        },
       }),
-    ).toEqual({
-      readValue: expect.any(Function),
-      writeValue: expect.any(Function),
-    });
+    ).toBeDefined();
+  });
+  it('returns bundle representation without config', () => {
+    expect(createUcBundle()).toBeDefined();
   });
 });
